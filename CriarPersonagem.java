2a0454437a61f5cd60eb21480989504bd0606c04
Maitê <81782090+maitecr@users.noreply.github.com>
2021-09-06 10:58:07 -0300

    Update CriarPersonagem.java

--- CriarPersonagem.java
+++ CriarPersonagem.java
@@ -1,23 +1,28 @@
+
 public class CriarPersonagens {
 
 	public static void main(String[] args) {
-		Knuckles knuckles = new Knuckles();
-		knuckles.setNome("Knuckles");
-		knuckles.coletarEsmeraldas();
-		knuckles.planar();
+		Tails tails = new Tails("Tails", 0.98, 40.0);
+		Knuckles knuckles = new Knuckles("Knuckles", 1.2, 60.0);
+		Sonic sonic = new Sonic("Sonic", 1.1, 80.0);
+		
+		tails.saltar();
+		sonic.saltar();
+		knuckles.saltar();
+		
+		tails.obstaculo(1.2);
+		sonic.obstaculo(2.3);
+		knuckles.obstaculo(0.98);
 		
 		
-		Sonic sonic = new Sonic();
-		sonic.setNome("Sonic");
-		sonic.coletarAneis();
-		sonic.entrarTails();
 		sonic.status();
+		sonic.correr();
+		sonic.status();
+		sonic.entrarTails(tails);
+		sonic.voarTails();
+		sonic.entrarTails(null);
+		sonic.voarTails();	
 		
-		Tails tails = new Tails();
-		tails.setNome("Tails");
-		tails.colecJS();
-		tails.colecJS();
-		tails.status();
 	}
 
 }
